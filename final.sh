c55aaba76478b22fa94fe2a3a46f64f6502df735
Deon4916 <95711287+Deon4916@users.noreply.github.com>
2021-12-13 21:49:41 -0800

    Update final.sh

--- final.sh
+++ final.sh
@@ -1,27 +1,22 @@
-Page up
 #!/bin/bash
 if [ -z "3"]
 then
         echo "
-        Usage:script [file extension]
+        Usage: script [file extension]
         [target directory][backup directory]
-        Example: script .text
-        /home/arnold/backup
+        Example: script .text /home/arnold/backup
         this is backs up files from a certain directory to another"
         exit 0;
 fi
 fileExtension="$1"
 targetDir="$2"
 finaldestination="$3"
-echo "file extension is:$fileextension"
+echo "file extension is: $fileextension"
 echo "target dir is: $targetDir"
-echo "backup directory is:
-$finaldestinationDir"
+echo "backup directory is: $finaldestinationDir"
 if [! -d"$targetDir]
 then
-        echo"$targetDir"]
+        echo "$targetDir"]
         exit 0;
 fi
-find $targetDir -name
-"*$fileExtension" -exec cp {}
-$filedestinationDir\
+find $targetDir -name "*$fileExtension" -exec cp {} $filedestinationDir\
